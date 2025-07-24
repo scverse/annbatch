88b0eb72ada202ae8f7f697d60b62e6662ee3d52
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-25 01:20:57 +0200

    🔖 Release 0.0.3

--- arrayloaders/__init__.py
+++ arrayloaders/__init__.py
@@ -15,6 +15,6 @@ The package has two modules.
 
 from __future__ import annotations
 
-__version__ = "0.0.2"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.0.3"  # denote a pre-release for 0.1.0 with 0.1rc1
 
 from . import io
