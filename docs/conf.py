85814008aad64ca396dfa6754a0080fa9e731eb0
Lukas Heumos <lukas.heumos@posteo.net>
2025-10-30 15:00:18 +0100

    fix copyrigh
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, str(HERE / "extensions"))
 info = metadata("annbatch")
 project_name = info["Name"]
 author = info["Author"]
-copyright = f"{datetime.now():%Y}, {author}."
+copyright = f"{datetime.now():%Y}, {author}"
 version = info["Version"]
 urls = dict(pu.split(", ") for pu in info.get_all("Project-URL"))
 repository_url = urls["Source"]
