0628019e933237fb2e277ecd9701c8f3015a0687
Felix Fischer <47145207+felix0097@users.noreply.github.com>
2026-02-10 18:34:13 +0100

    feat: Add var handling + validation (#133)
    * Add var handling + validation
    
    * Bug fix
    
    * only load .var index by default
    
    * Update src/annbatch/loader.py
    
    Co-authored-by: Ilan Gold <ilanbassgold@gmail.com>
    
    * Update src/annbatch/loader.py
    
    Co-authored-by: Ilan Gold <ilanbassgold@gmail.com>
    
    * Update load funcion
    
    * Use subtests for var test + improve docs
    
    * Apply suggestion from @ilan-gold
    
    ---------
    
    Co-authored-by: Ilan Gold <ilanbassgold@gmail.com>

--- src/annbatch/types.py
+++ src/annbatch/types.py
@@ -52,8 +52,9 @@ class LoadRequest(TypedDict):
 
 
 class LoaderOutput[OutputInMemoryArray: OutputInMemoryArray_T](TypedDict):
-    """The output of the loader, the "data matrix" with its obs, optional, and index, also optional."""
+    """The output of the loader, the "data matrix" with its obs, optional, var, optional, and index, also optional."""
 
     X: OutputInMemoryArray_T.__value__  # TODO: remove after sphinx 9 - myst compat
     obs: pd.DataFrame | None
+    var: pd.DataFrame | None
     index: np.ndarray | None
