70488361700ecb03414419bfe9ac2b4ff9c26c49
ilan-gold <ilanbassgold@gmail.com>
2026-03-26 11:44:48 +0100

    fix: better message

--- src/annbatch/__init__.py
+++ src/annbatch/__init__.py
@@ -8,9 +8,9 @@ from packaging.version import Version
 if find_spec("cupy"):
     import cupy as cp
 
-    cuda_version = cp.cuda.runtime.runtimeGetVersion()
+    cupy_expected_runtime_cuda_version = cp.cuda.runtime.runtimeGetVersion()
     # Is this safe?
-    if int(str(cuda_version)[:2]) != version("cuda-toolkit")[0]:
+    if int(str(cupy_expected_runtime_cuda_version)[:2]) != version("cuda-toolkit")[0]:
         msg = (
             "Found mismatched `cupy` compiled version and `cuda-toolkit` version."
             "For example, cupy-cuda12x requires torch < 2.11, which ships with cuda 12 by default, or >=2.11 with cuda 12 because >=2.11 ships with cuda 13 by default."
